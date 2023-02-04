23f42d814a93831816ed60b354ab581118eb3a42
Guiga-Silva <guiga.silvaa2@gmail.com>
2023-02-04 20:04:51 -0300

    first commit

---
+++ WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Guilherme Silva on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
